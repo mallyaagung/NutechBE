95789b6c5bbc40172d4cbca77fada4aa111259cd
mallyaagung <Mallyaagung@gmail.com>
2023-08-05 23:24:12 +0700

    vercel deploy

--- configs/db.js
+++ configs/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const db = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = db;
